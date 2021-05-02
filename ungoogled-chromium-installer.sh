38702e52431ade346a9da4a46d369863d2696dea
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-05-02 23:41:01 +0200

    Rewrite installer

--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Install script started"
 TAG="90.0.4430.85-1.1"
 
 # Install latest version
+echo "Download latest version"
 wget https://github.com/Joffreybvn/ungoogled-chromium-snapcraft/releases/download/${TAG}-sandbox/ungoogled-chromium_${TAG}-sandbox_amd64.snap
+
+echo "Snap install"
 sudo snap install ungoogled-chromium_${TAG}-sandbox_amd64.snap --dangerous
+rm ungoogled-chromium_${TAG}-sandbox_amd64.snap
 
 # Enable sandbox
+echo "Enable sandbox"
 sudo snap connect ungoogled-chromium:browser-sandbox
 
 echo "Ungoogled Chromium installation success !"
