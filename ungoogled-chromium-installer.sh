33f72c0a7285b7074d86d040ddc074a8f968e934
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-05-02 23:33:07 +0200

    Elevated privileges removed

--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -3,10 +3,6 @@
 echo "Install script started"
 TAG="90.0.4430.85-1.1"
 
-# Run with elevated privileges
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-
-
 # Install latest version
 wget https://github.com/Joffreybvn/ungoogled-chromium-snapcraft/releases/download/${TAG}-sandbox/ungoogled-chromium_${TAG}-sandbox_amd64.snap
 sudo snap install ungoogled-chromium_${TAG}-sandbox_amd64.snap --dangerous
