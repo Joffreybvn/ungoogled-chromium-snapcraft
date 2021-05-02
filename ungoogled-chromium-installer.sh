b59e5013b6a7c5e490925b0e918634ec9c85b92a
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-05-02 23:24:47 +0200

    Download link changed

--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+TAG="90.0.4430.85-1.1"
+
 # Run with elevated privileges
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 
 # Install latest version
-sudo snap install ungoogled-chromium_90.0.4430.85-1.1-sandbox_amd64.snap --dangerous
+wget https://github.com/Joffreybvn/ungoogled-chromium-snapcraft/releases/download/${TAG}-sandbox/ungoogled-chromium_${TAG}-sandbox_amd64.snap
+sudo snap install ungoogled-chromium_${TAG}-sandbox_amd64.snap --dangerous
 
 # Enable sandbox
 sudo snap connect ungoogled-chromium:browser-sandbox
