4a67ef4ebfc4ce658626f782ce3bc227cc792f5c
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-05-13 17:36:07 +0200

    Sandboxed version created

--- ungoogled-chromium-installer.sh
+++ ungoogled-chromium-installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TAG="90.0.4430.85-1.1"
+TAG="90.0.4430.212-1.1-sandbox"
 
 # Install latest version
 echo "Download latest version"
-wget https://github.com/Joffreybvn/ungoogled-chromium-snapcraft/releases/download/${TAG}-sandbox/ungoogled-chromium_${TAG}-sandbox_amd64.snap
+wget https://github.com/Joffreybvn/ungoogled-chromium-snapcraft/releases/download/${TAG}/ungoogled-chromium_${TAG}_amd64.snap
 
 echo "Snap install"
-sudo snap install ungoogled-chromium_${TAG}-sandbox_amd64.snap --dangerous
-rm ungoogled-chromium_${TAG}-sandbox_amd64.snap
+sudo snap install ungoogled-chromium_${TAG}_amd64.snap --dangerous
+rm ungoogled-chromium_${TAG}_amd64.snap
 
 # Enable sandbox
 echo "Enable sandbox"
